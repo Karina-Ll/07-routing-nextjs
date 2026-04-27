e413cb6fe22f8f800fa0d47db02b54dc4bcfa5c7
Karina <karina24032006.09@gmail.com>
2026-04-27 23:01:35 +0300

    Change files

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,33 +1,10 @@
-"use client";
+import NotePreviewClient from "./NotePreview.client";
 
-import { useRouter } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-import { fetchNoteById } from "../../../../lib/api";
-import Modal from "../../../../components/Modal/Modal";
-import NotePreview from "../../../../components/NotePreview/NotePreview";
-
-interface InterceptedNotePageProps {
-  params: { id: string };
+interface PageProps {
+  params: Promise<{ id: string }>;
 }
 
-export default function InterceptedNotePage({
-  params,
-}: InterceptedNotePageProps) {
-  const router = useRouter();
-  const { id } = params;
-
-  const { data: note, isLoading } = useQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
-  });
-
-  return (
-    <Modal onClose={() => router.back()}>
-      {isLoading || !note ? (
-        <p style={{ padding: "2rem" }}>Loading...</p>
-      ) : (
-        <NotePreview note={note} />
-      )}
-    </Modal>
-  );
+export default async function ModalNotePage({ params }: PageProps) {
+  const { id } = await params;
+  return <NotePreviewClient id={id} />;
 }
